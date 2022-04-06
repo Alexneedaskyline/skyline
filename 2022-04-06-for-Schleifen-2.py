3bcfcaf664af792268975181b925ee68413f330a
Alexneedaskyline <alexanderzeidler630@gmail.com>
2022-04-06 18:59:02 +0200

    rthrt
    hteh

---
+++ 2022-04-06-for-Schleifen-2.py
@@ -0,0 +1,20 @@
+#for i in range(26):
+ #   print(i)
+
+#Eine for-Schleife, die alle Zahlen von 10 bis 60 ausgibt
+#for i in range(51):
+  #  print(10 + i)
+
+#eine for Schleife, die alle Zahlen von 0 bis 100 in 2er-Schritten ausgibt
+#for i in range(51):
+    #print(i * 2)
+
+#eine for Schleife, die alle Zahlen von 50 bis 150 in 5er-Schritten ausgibt
+#for i in range(101):
+   # print(50+i)
+
+#eine for Schleife, die alle zahlen von 40 - 0 ausgibt
+
+#for i in range(41):
+ #   print(-i+40)
+
